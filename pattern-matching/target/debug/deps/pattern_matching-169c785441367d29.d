442882e270e17ad6e13d512fafd7c106e1873d21
Jucci16 <47046607+Jucci16@users.noreply.github.com>
2023-11-13 18:15:23 -0500

    Delete pattern-matching/target directory

--- pattern-matching/target/debug/deps/pattern_matching-169c785441367d29.d
+++
@@ -1,5 +0,0 @@
-C:\Users\aucci\source\repos\Jucci16\comprehensive-rust\pattern-matching\target\debug\deps\pattern_matching-169c785441367d29.rmeta: src\main.rs
-
-C:\Users\aucci\source\repos\Jucci16\comprehensive-rust\pattern-matching\target\debug\deps\pattern_matching-169c785441367d29.d: src\main.rs
-
-src\main.rs:
--- pattern-matching/target/debug/deps/pattern_matching-cf43bc0746b987ec.d
+++
@@ -1,5 +0,0 @@
-C:\Users\aucci\source\repos\Jucci16\comprehensive-rust\pattern-matching\target\debug\deps\pattern_matching-cf43bc0746b987ec.exe: src\main.rs
-
-C:\Users\aucci\source\repos\Jucci16\comprehensive-rust\pattern-matching\target\debug\deps\pattern_matching-cf43bc0746b987ec.d: src\main.rs
-
-src\main.rs:
