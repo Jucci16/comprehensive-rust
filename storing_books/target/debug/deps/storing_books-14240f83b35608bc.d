5fe22aaf42e54dcfaee0d0a772383684942ff6f2
Jucci16 <47046607+Jucci16@users.noreply.github.com>
2023-11-13 18:15:09 -0500

    Delete storing_books/target directory

--- storing_books/target/debug/deps/storing_books-14240f83b35608bc.d
+++
@@ -1,5 +0,0 @@
-C:\Users\aucci\source\repos\Jucci16\comprehensive-rust\storing_books\target\debug\deps\storing_books-14240f83b35608bc.rmeta: src\main.rs
-
-C:\Users\aucci\source\repos\Jucci16\comprehensive-rust\storing_books\target\debug\deps\storing_books-14240f83b35608bc.d: src\main.rs
-
-src\main.rs:
